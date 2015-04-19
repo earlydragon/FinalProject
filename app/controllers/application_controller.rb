69a52ce7b8aaaf020de9855fffc9ab9b8bc6f2c1
earlydragon <ddesai@collegiateschool.org>
2015-04-19 13:33:27 -0400

    New Edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,33 @@ class ApplicationController < Sinatra::Base
     set :session_secret, 'flatironrulz'
   end
 
-  get '/' do
+  get '/home' do
     erb :index
   end
+
+    get '/females' do
+    erb :females
+  end
+
+    get '/males' do
+    erb :males
+  end
+
+  	get '/fpl' do 
+  	erb :fpl
+  end 
+
+  	get '/mpl' do 
+  	erb :mpl
+  end
+
+  	get '/mdq' do 
+  	erb :mdq
+  end 
+
+  	get '/fdq' do 
+  	erb :fdq
+  end 
+
 end
+
