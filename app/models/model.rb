dc4637ee4257f3e7e77c94a817a4790c1380a9b6
thebasedgod1 <z.camille65@gmail.com>
2015-04-19 13:50:02 -0400

    Buddy Bear

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,26 @@
 require './config/environment'
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper
+  def initialize
+    html = open("http://www.pickuplinesgalore.com/cheesy.html")
+    @nokogiri1 = Nokogiri::HTML(html)
+    html_1 = open("http://www.pickuplinesgalore.com/women.html")
+    @nokogiri2 = Nokogiri::HTML(html_1)
+    html_3= open("http://www.pickuplinesgalore.com/computer.html")
+    @nokogiri3 = Nokogiri::HTML(html_3)
+  end
+
+def cheesy
+    @cheesy = @nokogiri1.css("style25")
+  end
+
+  def women
+    @women = @nokogiri2.css("style25")
+  end
+
+  def computer
+    @computer = @nokogiri3.css("style25")
+  end
+end
